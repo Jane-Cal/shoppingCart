82318d68047e671998cd5d523b4a8bff3d9d6a42
Jane-Cal <40821086+Jane-Cal@users.noreply.github.com>
2021-12-07 15:07:27 +0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Jane</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	</head>
+	<body>
+        <header>
+            <div class="content-wrapper">
+                <div class="container"> 
+                    <div class="row justify-content-end">
+                        <button type="button" data-toggle="modal" data-target="#cart-modal" class="cart btn btn-primary">My Cart (<span class="total-count"></span>)</button>
+                    </div>
+                </div>  
+            </div>
+        </header>
+        <main>
